2b4bcba6710d143c29a77fc02aa0c875cd318c5d
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-06-18 17:30:03 +0800

    feat: Add additional layouts and micro info in login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,34 +3,47 @@ import React from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import Logo from '@/components/Logo'
+import Link from 'next/link'
+import { Label } from "@/components/ui/label"
 
 const LoginPage = () => {
   return (
     <div className="min-h-screen flex bg-background">
-      <div className="p-8 hidden flex-1 bg-slate-500 md:block">
+      <div className="p-8 hidden flex-1 bg-slate-900 md:block">
         <div className="flex justify-between">
         </div>
       </div>
 
-      <div className="p-8  flex-1 flex flex-col">
+      <div className="p-8 flex-1 flex flex-col ">
         <div className="flex justify-between md:justify-end">
           <Logo className="md:hidden" />
-          <Button variant="outline">Sign Up</Button>
+          <Button variant="outline" asChild>
+            <Link href={"/signup"}>Sign Up</Link>
+          </Button>
         </div>
 
-        {/* TODO: Remove negative margin hack */}
-        <div className="mt-[-50px] flex-1 flex flex-col justify-center gap-7">
+        <div className='flex flex-col items-center w-full h-full justify-center gap-12 mb-[70px]'>
+          {/* Title */}
           <div className="flex flex-col gap-2">
-            <h2 className="font-bold text-3xl">Sign In</h2>
+            <h2 className="font-bold text-3xl text-center">Sign In</h2>
             <p>Enter your account information to sign in to your account.</p>
           </div>
 
-          <div className="flex justify-center">
+          {/* Form */}
+          <div className="flex justify-center w-full">
             <div className="max-w-96 flex-1 flex flex-col gap-7">
               <div className="flex flex-col gap-2">
-                <Input type="email" placeholder="Email" />
-                <Input type="password" placeholder="Password" />
-                <Button>Sign In with Email</Button>
+                <div className='gap-2'>
+                  <Label htmlFor="email">Email</Label>
+                  <Input type="email" placeholder="Email" />
+                </div>
+                <div className='gap-2'>
+                  <Label htmlFor="email">Password</Label>
+                  <Input type="password" placeholder="Password" />
+                </div>
+              </div>
+              <div className='w-full'>
+                <Button className='w-full'>Sign In with Email</Button>
               </div>
 
               {/* <div className="flex items-center gap-2">
