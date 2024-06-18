37901d0f9743c970a7a585caeca3486f482c2539
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-06-18 20:54:46 +0800

    feat: Add forms page

---
+++ app/(overview)/events/[eventId]/forms/[formId]/formsTable.tsx
@@ -0,0 +1,141 @@
+'use client';
+
+import { useState } from 'react';
+
+import { ColumnDef } from '@tanstack/react-table';
+import DataTable, {
+  DeletePopup,
+  SortableHeader,
+  getFormattedDate,
+} from '@/components/DataTable';
+import EditEventDialog from '@/components/EditEventDialog';
+import { Badge } from '@/components/ui/badge'
+import { badgeVariants } from '@/components/ui/badge';
+import { VariantProps } from 'class-variance-authority';
+
+const TEMP_COLUMNS: ColumnDef<unknown, any>[] = [
+  {
+    accessorKey: 'name',
+    header: ({ column }) => (
+      <SortableHeader column={column}>Name</SortableHeader>
+    ),
+  },
+  {
+    accessorKey: 'type',
+    header: ({ column }) => {
+
+      return <SortableHeader column={column}>Name</SortableHeader>
+    },
+    cell: ({ row }) => {
+      type BadgeVariant = VariantProps<typeof badgeVariants>['variant']
+      let badgeVariant: BadgeVariant = 'blue'
+      switch (row.getValue('type')) {
+
+        case "Expense":
+          badgeVariant = "blue"
+          break;
+        case "Revenue":
+          badgeVariant = "yellow"
+          break;
+        case "Fund Transfer":
+          badgeVariant = "destructive"
+          break;
+        case "Activity Transfer":
+          badgeVariant = "green"
+          break;
+      }
+      return <Badge variant={badgeVariant}>{row.getValue('type')}</Badge>
+    }
+  },
+  {
+    accessorKey: 'dateCreated',
+    header: ({ column }) => (
+      <SortableHeader column={column}>Date Created</SortableHeader>
+    ),
+    cell: ({ row }) => getFormattedDate(new Date(row.getValue('dateCreated'))),
+  },
+  {
+    accessorKey: 'dateModified',
+    header: ({ column }) => (
+      <SortableHeader column={column}>Date Modified</SortableHeader>
+    ),
+    cell: ({ row }) => getFormattedDate(new Date(row.getValue('dateModified'))),
+  },
+];
+
+const TEMP_DATA = [
+  {
+    name: "Form 1",
+    type: "Expense",
+    dateCreated: Date.now(),
+    dateModified: Date.now(),
+  },
+  {
+    name: "Form 2",
+    type: "Revenue",
+    dateCreated: Date.now(),
+    dateModified: Date.now(),
+  },
+  {
+    name: "Form 3",
+    type: "Fund Transfer",
+    dateCreated: Date.now(),
+    dateModified: Date.now(),
+  },
+  {
+    name: "Form 4",
+    type: "Activity Transfer",
+    dateCreated: Date.now(),
+    dateModified: Date.now(),
+  },
+]
+
+type EventsTableProps = {
+  nameFilter: string;
+  onDelete?: () => void;
+  onEdit?: () => void;
+  onSelect?: () => void;
+};
+
+const FormsTable = ({
+  nameFilter,
+  onDelete,
+  onEdit,
+  onSelect,
+}: EventsTableProps) => {
+  const [toDeleteId, setToDeleteId] = useState('');
+  const [toEditId, setToEditId] = useState('');
+
+  return (
+    <>
+      <DataTable
+        className="border-2"
+        clickableIdColumn={true}
+        columns={TEMP_COLUMNS}
+        data={TEMP_DATA}
+        idFilter={nameFilter}
+        idColumn="name"
+        onRowEdit={() => setToEditId('123')}
+        onRowDelete={() => setToDeleteId('123')}
+        onRowSelect={onSelect ?? (() => { })}
+      />
+
+      <DeletePopup
+        type="Event"
+        open={!!toDeleteId}
+        onCancel={() => setToDeleteId('')}
+        onConfirm={onDelete ?? (() => { })}
+      />
+
+      <EditEventDialog
+        isEditing={true}
+        open={!!toEditId}
+        onCancel={() => setToEditId('')}
+        onConfirm={onEdit ?? (() => { })}
+      />
+    </>
+  );
+};
+
+export default FormsTable;
+
--- app/(overview)/events/[eventId]/forms/page.tsx
+++ app/(overview)/events/[eventId]/forms/page.tsx
@@ -1,5 +1,65 @@
-const page = () => {
-  return <div>page</div>;
+'use client'
+
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { CirclePlus, SlidersHorizontal } from 'lucide-react';
+import FormsTable from './[formId]/formsTable';
+import EditEventDialog from '@/components/EditEventDialog';
+import EventJumpPointDialog from '@/components/EventJumpPointDialog';
+
+const EventsPage = () => {
+  const [nameFilter, setNameFilter] = useState('');
+
+  const [showCreateEventDialog, setShowCreateEventDialog] = useState(false);
+  const [showEventJumpPtDialog, setShowEventJumpPtDialog] = useState(false);
+
+  return (
+    <>
+      <main className="flex flex-col gap-4 px-6 py-4 text-left">
+        <div>
+          <h2 className="text-2xl font-bold">Forms for: Borther Richie’s secret finance forms and transactions</h2>
+          <p>Created: July 5, 2003 | Last Modified: July 5, 2003</p>
+        </div>
+
+        <div className="flex justify-between">
+          <Button onClick={() => setShowCreateEventDialog(true)}>
+            <CirclePlus className="mr-2 w-4" /> Create Event
+          </Button>
+
+          <div className="flex max-w-96 flex-1 gap-4">
+            <Input
+              placeholder="Search events by name..."
+              onChange={e => setNameFilter(e.target.value)}
+            />
+
+            <Button>
+              <SlidersHorizontal className="mr-2 w-4" /> Filter
+            </Button>
+          </div>
+        </div>
+
+        <FormsTable
+          nameFilter={nameFilter}
+          onSelect={() => setShowEventJumpPtDialog(true)}
+        />
+      </main>
+
+      <EditEventDialog
+        isEditing={false}
+        open={showCreateEventDialog}
+        onCancel={() => setShowCreateEventDialog(false)}
+        onConfirm={() => { }}
+      />
+
+      <EventJumpPointDialog
+        open={showEventJumpPtDialog}
+        onExit={() => setShowEventJumpPtDialog(false)}
+      />
+    </>
+  );
 };
 
-export default page;
+export default EventsPage;
+
