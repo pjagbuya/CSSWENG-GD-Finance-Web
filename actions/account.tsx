7a6ecfe1609b54e7acd61cff2843cfdd26b5f613
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-06-21 22:03:21 +0800

    feat: setup account server actions

---
+++ actions/account.tsx
@@ -0,0 +1,57 @@
+'use server'
+
+import { UserSchema } from "@/lib/definitions";
+import { redirect } from "next/navigation"
+import { revalidatePath } from "next/cache";
+
+export type State = {
+  errors?: {
+    email: string,
+    password: string,
+    first_name: string,
+    last_name: string,
+    role: string
+  };
+  message?: string | null;
+}
+
+export async function createAccount(prevState: State, formData: FormData) {
+  const validatedFields = UserSchema.safeParse(Object.fromEntries(formData.entries()))
+
+  if (!validatedFields.success) {
+    console.log(validatedFields.error)
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing fields. Unable to create event."
+    }
+  }
+
+  // TODO: provide logic
+
+  revalidatePath("")
+  redirect("/")
+}
+
+export async function editAccount(prevState: State, formData: FormData) {
+  const validatedFields = UserSchema.safeParse(Object.fromEntries(formData.entries()))
+
+  if (!validatedFields.success) {
+    console.log(validatedFields.error)
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing fields. Unable to edit event."
+    }
+  }
+
+  // TODO: provide logic
+
+  revalidatePath("")
+  redirect("/")
+}
+
+export async function deleteAccount(id: string) {
+
+  // TODO: provide logic
+
+  revalidatePath("")
+}
