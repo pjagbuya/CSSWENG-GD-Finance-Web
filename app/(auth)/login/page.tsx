15c677a987863a0c684943d9065604fc9a609c0e
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-06-24 15:55:15 +0800

    chore: convert login form to a client component

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,6 @@
-import React from 'react'
-
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
 import Logo from '@/components/Logo'
 import Link from 'next/link'
-import { Label } from "@/components/ui/label"
 
 const LoginPage = () => {
   return (
@@ -30,33 +26,6 @@ const LoginPage = () => {
           </div>
 
           {/* Form */}
-          <div className="flex justify-center w-full">
-            <div className="max-w-96 flex-1 flex flex-col gap-7">
-              <div className="flex flex-col gap-2">
-                <div className='flex flex-col gap-2'>
-                  <Label htmlFor="email">Email</Label>
-                  <Input type="email" placeholder="Email" />
-                </div>
-                <div className='flex flex-col gap-2'>
-                  <Label htmlFor="email">Password</Label>
-                  <Input type="password" placeholder="Password" />
-                </div>
-              </div>
-              <div className='w-full'>
-                <Button className='w-full'>Sign In with Email</Button>
-              </div>
-
-              {/* <div className="flex items-center gap-2">
-                <span className="flex-1 border-[1px] border-border h-[1px]"></span>
-                <p className="text-muted-foreground">OR CONTINUE WITH</p>
-                <span className="flex-1 border-[1px] border-border h-[1px]"></span>
-              </div>
-
-              <div className="flex flex-col gap-2">
-                <Button variant="outline">Google</Button>
-              </div> */}
-            </div>
-          </div>
         </div>
       </div>
     </div>
