ca98a8e1124a0717fb690cbe057ecaf6ccaad39d
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-06-24 17:16:16 +0800

    feat: add dynamic user email info in header component

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,15 +1,35 @@
+'use client'
+
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { createClient } from '@/utils/supabase/client';
+import { redirect } from 'next/navigation'
+import React from 'react'
 
 type LogoProps = {
   className?: string;
 };
 
 const Profile = ({ className }: LogoProps) => {
+  const [email, setEmail] = useState<string>('')
+  useEffect(() => {
+    async function getUser() {
+      const supabase = createClient()
+
+      const { data, error } = await supabase.auth.getUser()
+      if (error || !data?.user) {
+        redirect('/login')
+      }
+      setEmail(data.user.email || 'anonymous')
+    }
+
+    getUser()
+  })
   return (
     <div className={`${className} flex items-center gap-5`}>
       <div>
-        <p className="-mb-1.5 font-bold">Name Name Name Name</p>
+        <p className="-mb-1.5 font-bold">{email}</p>
         <small>Position Position</small>
       </div>
 
