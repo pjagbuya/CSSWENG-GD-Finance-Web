93ea5b655b1bc617d9fd1808984f06686d81bdc5
Clydie-Juls <clyde_marindo@dlsu.edu.ph>
2024-06-24 15:31:34 +0800

    feat: add form error messages

--- lib/definitions.tsx
+++ lib/definitions.tsx
@@ -1,15 +1,25 @@
 import { z } from "zod"
 
 export const UserSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6),
-  first_name: z.string(),
-  last_name: z.string(),
+  email: z.string({
+    required_error: 'Please enter your email.'
+  }).email(),
+  password: z.string({
+    required_error: 'Please enter your password.'
+  }).min(6, `Password must be at least 6 characters`),
+  first_name: z.string({
+    required_error: 'Please enter your first name.'
+  }),
+  last_name: z.string({
+    required_error: 'Please enter your last name.'
+  }),
   role: z.enum(['admin', 'member'])
 })
 
 export const EventSchema = z.object({
-  name: z.string()
+  name: z.string({
+    required_error: 'Please enter an event name.'
+  })
 })
 
 export const LoginForm = UserSchema.pick({ email: true, password: true })
