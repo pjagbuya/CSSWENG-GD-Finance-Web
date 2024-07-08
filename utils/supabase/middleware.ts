7859830125b1aa1f7fbd89d17531dcfdd9a8e1f1
clydie-juls <clyde_marindo@dlsu.edu.ph>
2024-07-08 21:15:38 +0800

    feat: add page page protection(admin-only access) to accounts page

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -1,4 +1,5 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { error } from 'console'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
@@ -54,9 +55,24 @@ export async function updateSession(request: NextRequest) {
     }
   )
 
-  const { error } = await supabase.auth.getUser()
-  if (error) {
+  const { data: { user }, error: authError } = await supabase.auth.getUser()
+  if (authError) {
     return NextResponse.redirect(new URL('/login', request.url))
+  }
+
+  const { data: userData, error: userError } = await supabase
+    .from('users_view')
+    .select('*')
+    .eq('uuid', user?.id)
+
+  if (request.nextUrl.pathname.startsWith('/account')) {
+    if (userError) {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    if (userData[0]?.staff_position !== 'chief') {
+      return NextResponse.redirect(new URL('/', request.url))
+    }
   }
 
   return response
