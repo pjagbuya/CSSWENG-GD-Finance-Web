2244de196a46734e2ad0fd8c7061747c32511cde
clydie-juls <clyde_marindo@dlsu.edu.ph>
2024-07-20 19:31:21 +0800

    fix: remove duplicate zod schema

--- lib/definitions.tsx
+++ lib/definitions.tsx
@@ -334,29 +334,6 @@ export const StaffListSchema = z.object({
     .min(1),
 });
 
-export const StaffSchema = z.object({
-  staff_id: z
-    .string({
-      required_error: 'Please enter Staff ID.',
-    })
-    .min(1),
-  staff_name: z
-    .string({
-      required_error: 'Please enter Staff name.',
-    })
-    .min(1),
-  staff_position: z
-    .string({
-      required_error: 'Please enter Staff position.',
-    })
-    .min(1),
-  staff_list_id: z
-    .string({
-      required_error: 'Please enter Staff list ID.',
-    })
-    .min(1),
-});
-
 export const TransactionSchema = z.object({
   transaction_id: z
     .string({ required_error: 'Please enter Transaction ID.' })
