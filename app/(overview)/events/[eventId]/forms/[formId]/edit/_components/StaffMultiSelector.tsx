b1eeabc579ac0d7548e25e3bd35ca281e4fe8eb7
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-07-23 23:45:39 +0800

    Fix staff checkbox

--- app/(overview)/events/[eventId]/forms/[formId]/edit/_components/StaffMultiSelector.tsx
+++ app/(overview)/events/[eventId]/forms/[formId]/edit/_components/StaffMultiSelector.tsx
@@ -1,10 +1,10 @@
-import { getUser } from "@/actions/account";
-import { selectWhereStaffInstanceValidation } from "@/actions/staff_instances";
-import { selectAllStaff, selectWhereStaffValidation } from "@/actions/staffs";
-import { getStaffInfos } from "@/actions/utils";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Label } from "@/components/ui/label";
-import { useEffect, useState } from "react";
+import { getUser } from '@/actions/account';
+import { selectWhereStaffInstanceValidation } from '@/actions/staff_instances';
+import { selectAllStaff, selectWhereStaffValidation } from '@/actions/staffs';
+import { getStaffInfos } from '@/actions/utils';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Label } from '@/components/ui/label';
+import { useEffect, useState } from 'react';
 
 type StaffMultiSelectorProps = {
   label: string;
@@ -20,8 +20,8 @@ const StaffMultiSelector = ({ label, value }: StaffMultiSelectorProps) => {
   useEffect(() => {
     const fetchStaffList = async () => {
       const response = await selectAllStaff();
-      const list = await getStaffInfos(response) || []
-      
+      const list = (await getStaffInfos(response)) || [];
+
       setStaffList(list);
     };
 
@@ -32,42 +32,54 @@ const StaffMultiSelector = ({ label, value }: StaffMultiSelectorProps) => {
     fetchData();
 
     async function fetchData() {
-      const data = await selectWhereStaffInstanceValidation(value, 'staff_instance_id');
+      const data = await selectWhereStaffInstanceValidation(
+        value,
+        'staff_list_id',
+      );
       const staffInstances = data!.data!;
-      
-      console.log(value, staffInstances)
 
-      const result = await Promise.all(staffInstances
-        .filter((staffInstance: any) => staffInstance.staff_list_id === value)
-        .map(async (staffInstance: any) => {
-          const info = await selectWhereStaffValidation(staffInstance.staff_id, 'staff_id');
-          
-          return {
-            ...staffInstance,
-            ...(info.data)
-          };
-        }));
+      const result = staffInstances.map(s => s.staff_id);
+
+      // const result = await Promise.all(
+      //   staffInstances
+      //     .filter((staffInstance: any) => staffInstance.staff_list_id === value)
+      //     .map(async (staffInstance: any) => {
+      //       const info = await selectWhereStaffValidation(
+      //         staffInstance.staff_id,
+      //         'staff_id',
+      //       );
+
+      //       return {
+      //         ...staffInstance,
+      //         ...info.data,
+      //       };
+      //     }),
+      // );
+
+      console.log(result);
 
       setIncludedStaffIds(result);
     }
   }, [value]);
-  
+
   return (
     <>
       <Label>{label}</Label>
 
       {staffList.map((obj: any) => (
-        <div className="flex gap-2 items-center" key={obj.staff_id}>
-          <Checkbox 
-            checked={includedStaffIds.includes(obj.staff_id)} 
+        <div className="flex items-center gap-2" key={obj.staff_id}>
+          <Checkbox
+            checked={includedStaffIds.includes(obj.staff_id)}
             name={`noted_staff_list_id-${obj.staff_id}`}
             onCheckedChange={(v: boolean) => {
               if (v) {
                 setIncludedStaffIds(includedStaffIds.concat(obj.staff_id));
-                setStaffList(staffList)
+                setStaffList(staffList);
               } else {
-                setIncludedStaffIds(includedStaffIds.filter(x => x !== obj.staff_id));
-                setStaffList(staffList)
+                setIncludedStaffIds(
+                  includedStaffIds.filter(x => x !== obj.staff_id),
+                );
+                setStaffList(staffList);
               }
             }}
           />
